4a5e2a96700e1e1ec1e586edf9dd88dc131b0ffb
Kirill Larchik <lar4ik.kirill@outlook.com>
2020-10-17 17:20:07 +0300

    Added the figure collection and the method for read from file.

---
+++ testtask/FiguresLibrary/FiguresCollection.cs
@@ -0,0 +1,123 @@
+﻿using FiguresLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLibrary
+{
+    public enum Figures
+    {
+        Circle,
+        Rectangle,
+        Square
+    }
+
+    /// <summary>
+    /// Class describing a collection of figures.
+    /// </summary>
+    public class FiguresCollection
+    {
+        private List<IFigure> figures;
+
+        /// <summary>
+        /// The count of figures.
+        /// </summary>
+        public int Count { get { return figures.Count; } }
+
+        public IFigure this[int index]
+        {
+            get 
+            {
+                return figures[index];
+            }
+            set
+            {
+                figures[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Inits the new figures collection.
+        /// </summary>
+        public FiguresCollection()
+        {
+            figures = new List<IFigure>();
+        }
+
+        /// <summary>
+        /// Adds a new figure.
+        /// </summary>
+        /// <param name="figure"></param>
+        public void Add(IFigure figure)
+        {
+            figures.Add(figure);
+        }
+
+        /// <summary>
+        /// Removes a figure from the collection by index.
+        /// </summary>
+        /// <param name="index"></param>
+        public void Remove(int index)
+        {
+            figures.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Clears the collection.
+        /// </summary>
+        public void Clear()
+        {
+            figures.Clear();
+        }
+
+        /// <summary>
+        /// Reads txt file and returns the figures collection.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        public void ReadFromFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] vs = line.Split(' ');
+
+                    switch (Enum.Parse(typeof(Figures), vs[0]))
+                    {
+                        case Figures.Circle:
+                            int radius;
+                            if (!int.TryParse(vs[1], out radius))
+                                throw new Exception("Incorrect format of data.");
+                            
+                            Add(new Circle(radius));
+                            break;
+                        case Figures.Rectangle:
+                            int length;
+                            if (!int.TryParse(vs[1], out length))
+                                throw new Exception("Incorrect format of data.");
+                            
+                            int width;
+                            if(!int.TryParse(vs[2], out width))
+                                throw new Exception("Incorrect format of data.");
+                            
+                            Add(new Rectangle(length, width));
+                            break;
+                        case Figures.Square:
+                            int side;
+                            if (!int.TryParse(vs[1], out side))
+                                throw new Exception("Incorrect format of data.");
+
+                            Add(new Square(side));
+                            break;
+                        default:
+                            throw new Exception("Incorrect format of data.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ testtask/UnitTestProject/FiguresCollectionUT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using FiguresLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class FiguresCollectionUT
+    {
+        [TestMethod]
+        public void ReadFromFile()
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\files\figures.txt";
+            int exptected = File.ReadAllLines(filePath).Length;
+            
+            FiguresCollection collection = new FiguresCollection();
+            collection.ReadFromFile(filePath);
+            int actual = collection.Count;
+
+            Assert.AreEqual(exptected, actual);
+        }
+    }
+}
